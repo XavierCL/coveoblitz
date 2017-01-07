f3ed3fa621464c28b0b0fa7dcee9389d1755f6fd
jsletellier <js.letellier@gmail.com>
2017-01-07 10:09:04 -0500

    Some things

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const request = require('request')
 
 var getDirection = function(start, target, rawMap, size, callback) {
     var url = 'http://game.blitz.codes:8081/pathfinding/direction?size='+size+'&start=('+start.x+','+start.y+')&target=('+target.x+','+target.y+')&map='+encodeURIComponent(rawMap)
-    console.log(url)
+    // console.log(url)
     request(url, function (error, response, body) {
         callback(body.split("\"")[3])
     })
@@ -33,22 +33,37 @@ function bot(play, callback) {
         isFirstRound = false;
     }
 
-    // The board is an object containing the full map in a 2d array and the positions of the burgers, french fries, taverns and customers
-    // feel free to edit tile.js and board.js to make them meet your needs.
     let board = parseBoard(play.game.board);
 
     let map = board.map;
     let burgers = board.burgers;
     let frenchFries = board.frenchFries;
     let taverns = board.taverns;
-    let customers = board.customers;
 
-    console.log(play.game)
+    let customers = play.game.customers;
+    let customersPositions = board.customers;
+    let heros = play.game.heroes;
+    let myHero = getMyHero(heros);
 
     var nextDirection = getDirection(getMyHero(play.game.heroes).pos, burgers[0], play.game.board.tiles, play.game.board.size, function(direction) {
         console.log(direction);
         callback(null, dirs[direction])
     })
+    let nextCustomer = selectNextCustomer(customers);
+
+    // fullfillOrder(nextCustomer, burgers, frenchFries);
+
+    // console.log(getMyHero(heros));
+
+    console.log(myHero.pos);
+
+    if (nextDirection === 'n') {
+        var currentPosition = myHero.pos;
+        console.log(nextDirection);
+        console.log(map);
+    }
+
+    console.log(nextDirection);
 }
 
 function getMyHero(heroes) {
@@ -59,11 +74,33 @@ function getMyHero(heroes) {
     }
 }
 
+function selectNextCustomer(customers) {
+    var burgerToFrenchFriesRatio = 0;
+    var nextCustomerIndex = 0;
+
+    for (var i=0; i<customers.length; i++) {
+        var currentCustomer = customers[i];
+        var projectedRatio = (currentCustomer.burger) / (currentCustomer.frenchFries + 1);
+
+        if (projectedRatio > burgerToFrenchFriesRatio) {
+            nextCustomerIndex = i;
+        }
+    }
+
+    return customers[nextCustomerIndex];
+}
+
 function selectRandomDirection(dirs) {
     let i = Math.floor(Math.random() * 5);
     let nextDirection = dirs[i];
 
     return nextDirection;
+}
+
+function fullfillOrder(customer, burger, frenchFries) {
+    console.log(customer);
+    console.log(burger);
+    console.log(frenchFries);
 }
 
 module.exports = bot;
